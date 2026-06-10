72996c6cdfed22d97281fc194b43a44a8cc5ce94
Ansh Gajjar <anshgajjar2627@gmail.com>
2026-06-10 11:27:35 +0530

    Day 2 - Security Header Scanner

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/scan", methods=["POST"])
+def scan():
+    website = request.form.get("website")
+
+    try:
+        if not website.startswith(("http://", "https://")):
+            website = "https://" + website
+
+        response = requests.get(website, timeout=5)
+
+        headers = response.headers
+
+        security_headers = {
+            "Content-Security-Policy": headers.get("Content-Security-Policy"),
+            "Strict-Transport-Security": headers.get("Strict-Transport-Security"),
+            "X-Frame-Options": headers.get("X-Frame-Options"),
+            "X-Content-Type-Options": headers.get("X-Content-Type-Options"),
+        }
+
+        return render_template(
+            "result.html",
+            website=website,
+            security_headers=security_headers
+        )
+
+    except Exception as e:
+        return f"Error: {e}"
 
 if __name__ == "__main__":
     app.run(debug=True)
